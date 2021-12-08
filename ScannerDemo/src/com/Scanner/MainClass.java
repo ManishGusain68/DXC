ac3bd134555030f75d8f8ce30e903a80557c66e9
ManishGusain68 <gusainmanish68@gmail.com>
2021-12-08 17:24:41 +0530

    Scanner Demo

---
+++ ScannerDemo/src/com/Scanner/MainClass.java
@@ -0,0 +1,16 @@
+package com.Scanner;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String... arg) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("please enter the word ");
+		String str=scan.next();
+		System.out.println("Entered value is :"+ str);
+		
+
+	}
+
+}
