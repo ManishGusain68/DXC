274d565a08f4853e109a238c2dd806a1110b2e9b
ManishGusain68 <gusainmanish68@gmail.com>
2021-12-08 17:22:37 +0530

    data types demo

---
+++ DataTypesDemo/src/com/OperatorsDemo/ArtithDemo.java
@@ -0,0 +1,15 @@
+package com.OperatorsDemo;
+
+public class ArtithDemo {
+
+	public static void main(String[] args) {
+		int a=100;
+		
+		int b=200;
+		
+		int c=a++ + ++b;
+		
+		System.out.println(c);
+	}
+
+}
---
+++ DataTypesDemo/src/com/datatypesdemo/DataTypeTuto.java
@@ -0,0 +1,28 @@
+package com.datatypesdemo;
+
+public class DataTypeTuto {
+
+	public static void main(String[] args) {
+	
+		byte b=110;
+		
+		int i=b;  //implicit type casting
+		
+		
+		int m=100;
+		
+		byte n=(byte)m;  //explicit type casting
+		
+		System.out.println(n );
+		
+		
+		double a=50.50;
+		
+		float f=(float)a; //explicit type casting
+		
+	
+		
+
+	}
+
+}
