81071d1fb5130aa21e8e3fd232596e9e0efbd52d
ManishGusain68 <gusainmanish68@gmail.com>
2021-12-09 12:11:37 +0530

    spring dependency injection

---
+++ FIrstMavenProject/src/main/java/com/pack1/Address.java
@@ -0,0 +1,38 @@
+package com.pack1;
+
+public class Address {
+
+	private int pincode;
+	private String city;
+
+	public Address() {
+		super();
+		System.out.println("address object created");
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [pincode=" + pincode + ", city=" + city + "]";
+	}
+	
+	public void sayHi(){
+		System.out.println("Hi from Address class");
+	}
+
+}
---
+++ FIrstMavenProject/src/main/java/com/pack1/Student.java
@@ -0,0 +1,58 @@
+package com.pack1;
+
+public class Student {
+
+	private int id;
+	private String name;
+	private Address addr;   //has-a relationahip
+
+	public Address getAddr() {
+		return addr;
+	}
+
+	public void setAddr(Address addr) {
+		this.addr = addr;
+		System.out.println("setting addr");
+	}
+
+	public Student() {
+		super();
+		System.out.println("student object created");
+	}
+
+	// public Student(int id, String name) {
+	// super();
+	// this.id = id;
+	// this.name = name;
+	// }
+
+	public int getId() {
+		System.out.println("getting the id");
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+		System.out.println("setting the id");
+	}
+
+	public String getName() {
+		System.out.println("getting the name");
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+		System.out.println("setting the name");
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", addr=" + addr + "]";
+	}
+	
+	public void sayHello(){
+		System.out.println("hello from student");
+	}
+
+}
