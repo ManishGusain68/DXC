e1feae5f3e459fa6a44adab95d804d7b8294e9a0
ManishGusain68 <gusainmanish68@gmail.com>
2021-12-10 12:37:00 +0530

    ..

--- FIrstMavenProject/src/main/java/com/ConfigClassDemo/JavaConfig.java
+++ FIrstMavenProject/src/main/java/com/ConfigClassDemo/JavaConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages="com.ConfigClassDemo")
+
 public class JavaConfig {
 	@Bean
 	public Teacher getTeacher(){
