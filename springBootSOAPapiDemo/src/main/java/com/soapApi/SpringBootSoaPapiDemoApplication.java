7c049be6388cf981f1ac5a60436103b36882ccf6
ManishGusain68 <gusainmanish68@gmail.com>
2021-12-20 12:36:04 +0530

    soap webservice

---
+++ springBootSOAPapiDemo/src/main/java/com/soapApi/SpringBootSoaPapiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.soapApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoaPapiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoaPapiDemoApplication.class, args);
+	}
+
+}
---
+++ springBootSOAPapiDemo/src/test/java/com/soapApi/SpringBootSoaPapiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soapApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoaPapiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
